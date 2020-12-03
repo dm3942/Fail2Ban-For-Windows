e355390d55cb17ca494e332a22824312be304292
Daniel M <dm3942@users.noreply.github.com>
2020-12-04 00:27:19 +1100

    Updated Script and Added Images for Wiki

--- Fail2Ban_ForWindows.ps1
+++ Fail2Ban_ForWindows.ps1
@@ -1,4 +1,6 @@
-﻿
+﻿# https://github.com/dm3942/Fail2Ban-For-Windows
+# dm3942
+
 $IPAddresses = @{}
 
 $query = @"
@@ -16,6 +18,7 @@ $query = @"
 
 $startTime = Get-Date
 $lastIPaddr = "127.0.0.1"
+$outbuff = ""
 
 While (1) { # while (Get-Date) -lt $startTime.AddSeconds(20)) # run for 20 seconds
     $avent = Get-WinEvent -FilterXml $query -MaxEvents 1
@@ -23,12 +26,14 @@ While (1) { # while (Get-Date) -lt $startTime.AddSeconds(20)) # run for 20 secon
         $IPaddr = $avent.Properties[19].Value
         try { 
             $IPAddresses.Add($IPaddr,$IPaddr) 
-            Write-Host -ForegroundColor Red "$IPaddr  new attacker"
-            New-NetFirewallRule -Profile Any -DisplayName "Block $(Get-Date (Get-Date) -UFormat '%Y%m%d%H%M%S') $IPaddr" -RemoteAddress $IPaddr -Enabled True -Direction Inbound -Action Block # -InformationAction SilentlyContinue
+            Write-Host -ForegroundColor Red "$IPaddr  New attacker, time to block. $(Get-Date)"
+            Write-Host "    Creating rule"
+            New-NetFirewallRule -Profile Any -DisplayName "Block $(Get-Date (Get-Date) -UFormat '%Y%m%d%H%M%S') $IPaddr" -RemoteAddress $IPaddr -Enabled True -Direction Inbound -Action Block | % { Write-Host "    -> ", $_.DisplayName, "-", $_.Status }
+            Write-Host "    Rule created"
         } catch [Exception] {
             if($lastIPaddr -ne $IPaddr) {
                 $lastIPaddr = $IPaddr
-                Write-Host -ForegroundColor Green "$IPaddr  existing attacker"
+                Write-Host -ForegroundColor Green "$IPaddr  existing attacker, rule already exists. $(Get-Date)"
             }
             Sleep -Milliseconds 200
         }
