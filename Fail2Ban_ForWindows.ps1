ce97360fa055743a1fa1a54b7a5510b0347baa63
Daniel M <dm3942@users.noreply.github.com>
2020-12-04 01:24:24 +1100

    Completed To Do objectives
    Don't create rules for failed logon attempts from the same subnet.
    Clean up rules that are more than a day old.

--- Fail2Ban_ForWindows.ps1
+++ Fail2Ban_ForWindows.ps1
@@ -20,23 +20,49 @@ $startTime = Get-Date
 $lastIPaddr = "127.0.0.1"
 $outbuff = ""
 
+$lastcleanup = Get-Date
+
 While (1) { # while (Get-Date) -lt $startTime.AddSeconds(20)) # run for 20 seconds
     $avent = Get-WinEvent -FilterXml $query -MaxEvents 1
     if($avent.Properties.Count -gt 19) {
         $IPaddr = $avent.Properties[19].Value
-        try { 
+        try {
+            # Show IP Address 
             $IPAddresses.Add($IPaddr,$IPaddr) 
             Write-Host -ForegroundColor Red "$IPaddr  New attacker, time to block. $(Get-Date)"
             Write-Host "    Creating rule"
-            New-NetFirewallRule -Profile Any -DisplayName "Block $(Get-Date (Get-Date) -UFormat '%Y%m%d%H%M%S') $IPaddr" -RemoteAddress $IPaddr -Enabled True -Direction Inbound -Action Block | % { Write-Host "    -> ", $_.DisplayName, "-", $_.Status }
-            Write-Host "    Rule created"
+
+            # Check for local subnet
+            $skipplocal = $false
+            Get-NetIPAddress | where IPv4Address -ne $null | select IPAddress | % {
+                if( ($_.IPAddress -like "$($IPaddr.Substring(0,$IPaddr.LastIndexOf('.')))*")  ) {
+                    "    Local address skipping rule creation. "
+                    $skipplocal = $true
+                }
+            }
+
+            # Create Firewall Rule
+            if(-not $skipplocal) {
+                New-NetFirewallRule -Profile Any -DisplayName "Fail2Ban Block $(Get-Date (Get-Date) -UFormat '%Y%m%d%H%M%S') $IPaddr" -RemoteAddress $IPaddr -Enabled True -Direction Inbound -Action Block | % { Write-Host "    -> ", $_.DisplayName, "-", $_.Status }
+                Write-Host "    Rule created"
+            }
         } catch [Exception] {
+            # Show IP address
             if($lastIPaddr -ne $IPaddr) {
                 $lastIPaddr = $IPaddr
-                Write-Host -ForegroundColor Green "$IPaddr  existing attacker, rule already exists. $(Get-Date)"
+                Write-Host -ForegroundColor Green "$IPaddr  existing attacker, rule already exists. No new events found. $(Get-Date)"
             }
-            Sleep -Milliseconds 200
+            Sleep -Milliseconds 1000
         }
+    }
+
+    # Every hour, clean up old rules
+    if ( ((Get-Date)-$lastcleanup).Hour -gt 1) 
+    {
+        Write-Host -ForegroundColor DarkGreen "Cleaning up old rules. $(Get-Date)"
+        $yesterdays = Get-Date (Get-Date).AddDays(-1) -UFormat '%Y%m%d' 
+        Get-NetFirewallRule -DisplayName "Fail2Ban Block $($yesterdays)*" | Remove-NetFirewallRule 
+        $lastcleanup = Get-Date
     }
 }
 
